cd83fddf2320f05b8aaa22ef7919be40b16665cb
dnhoangson <dnhoangson.danang@gmail.com>
2020-09-23 15:39:14 +0700

    Added CongViecAccess CongViecBUS
    MarkStatus với
    status = true thì set status công việc là true và cập nhật NgayKetThuc là GETDATE()
    
    status = false thì set status công việc là false và cập nhật NgayKetThuc là NULL

---
+++ TLWebForm/App_Data/BAL/CongViecBUS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using TLWebForm.App_Data.DAL;
+
+namespace TLWebForm.App_Data.BAL
+{
+    public class CongViecBUS
+    {
+        private CongViecAccess service = new CongViecAccess();
+        public CongViecBUS() { }
+
+        public void MarkStatus(string id, bool status)
+        {
+            if(status == true)
+            {
+                service.MarkStatus(id, status);
+                service.UpdateFinishDate(id, "GETDATE()");
+            }
+            else
+            {
+                service.MarkStatus(id, status);
+                service.UpdateFinishDate(id, "NULL");
+            }
+        }
+    }
+}
