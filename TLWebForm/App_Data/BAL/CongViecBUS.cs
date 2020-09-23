7556e860962c1be5e4cb91607e1103bb059ada9f
dnhoangson <dnhoangson.danang@gmail.com>
2020-09-23 23:27:08 +0700

    Thêm IdNhanVien và IdPartner vào công việc

--- TLWebForm/App_Data/BAL/CongViecBUS.cs
+++ TLWebForm/App_Data/BAL/CongViecBUS.cs
@@ -25,5 +25,34 @@ namespace TLWebForm.App_Data.BAL
                 service.UpdateFinishDate(id, "NULL");
             }
         }
+
+        public bool AssignCongViec(string idCongViec, string idNhanVien, string idPartner)
+        {
+            if((int.Parse(idNhanVien)) <= 0 || (int.Parse(idCongViec)) <= 0)
+            {
+                return false;
+            }
+
+            if(int.Parse(idNhanVien) == int.Parse(idCongViec))
+            {
+                return false;
+            }
+
+            if(idNhanVien == null)
+            {
+                return false;
+            }
+
+            if(idNhanVien != null && idPartner == null)
+            {
+                service.AssignNhanVienToCongViec(idCongViec, idNhanVien);
+                service.AssignPartnerToCongViec(idCongViec, null);
+                return true;
+            }
+            service.AssignNhanVienToCongViec(idCongViec, idNhanVien);
+            service.AssignPartnerToCongViec(idCongViec, idPartner);
+            return true;
+            
+        }
     }
 }
