699934e03e80a2082789ae5b0e4c9082994b48b4
dnhoangson <dnhoangson.danang@gmail.com>
2020-09-24 13:53:57 +0700

    Tạo comment cho công việc

---
+++ TLWebForm/App_Data/BAL/CommentBUS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using TLWebForm.App_Data.DAL;
+
+namespace TLWebForm.App_Data.BAL
+{
+    public class CommentBUS
+    {
+        private CommentAccess service = new CommentAccess();
+        public CommentBUS() { }
+
+       public void MakeComment(string idCongViec, string idNhanVien, string content)
+        {
+            if(idCongViec == null || idNhanVien == null || content == null)
+            {
+                throw new ArgumentNullException("IdCongViec, IdNhanVien and Comment content cannot be null");
+            }
+            else
+            {
+                service.MakeComment(idCongViec, idNhanVien, content);
+            }
+        }
+        
+    }
+}
---
+++ TLWebForm/App_Data/DAL/CommentAccess.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace TLWebForm.App_Data.DAL
+{
+    public class CommentAccess
+    {
+        public void MakeComment(string idCongViec, string idNhanVien, string content)
+        {
+            string connectionString = DataAccess.Internal.DataAccess.GetConnectionString("TodoListDb");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = @"insert into Comment (IdCongViec, IdNhanVien, Content)" +
+                                "values (@idCongViec, @idNhanVien, @content)" +
+                                "where Id = @id";
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@idCongViec", idCongViec);
+                    cmd.Parameters.AddWithValue("@idNhanVien", idNhanVien);
+                    cmd.Parameters.AddWithValue("@content", content);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        
+    }
+}
